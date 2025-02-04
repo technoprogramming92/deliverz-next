db9206e35f4afee127a3b06d78c9cac98afb0f2c
Rachit Trivedi <technoprogramming92@gmail.com>
2025-02-04 15:04:39 +0530

    next config change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  productionBrowserSourceMaps: false
+  productionBrowserSourceMaps: false,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.html$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
